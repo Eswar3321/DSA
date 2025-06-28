9fb172cbd618dbd20997104e00bab3f90e1b6f1f
Eswar3321 <eswararao8786@gmail.com>
2025-06-28 21:35:43 +0530

    miidle node&reverse list

--- linkedlist.js
+++ linkedlist.js
@@ -1,27 +1,24 @@
+// design linked list
 function Node(val) {
     this.val = val;
     this.next = null;
 }
-
 var MyLinkedList = function() {
     this.head = null;
     this.size = 0;
 };
-
 MyLinkedList.prototype.get = function(index) {
     if (index < 0 || index >= this.size) return -1;
     let curr = this.head;
     for (let i = 0; i < index; i++) curr = curr.next;
     return curr.val;
 };
-
 MyLinkedList.prototype.addAtHead = function(val) {
     const newNode = new Node(val);
     newNode.next = this.head;
     this.head = newNode;
     this.size++;
 };
-
 MyLinkedList.prototype.addAtTail = function(val) {
     const newNode = new Node(val);
     if (!this.head) {
@@ -33,7 +30,6 @@ MyLinkedList.prototype.addAtTail = function(val) {
     }
     this.size++;
 };
-
 MyLinkedList.prototype.addAtIndex = function(index, val) {
     if (index < 0 || index > this.size) return;
 
@@ -50,7 +46,6 @@ MyLinkedList.prototype.addAtIndex = function(index, val) {
         this.size++;
     }
 };
-
 MyLinkedList.prototype.deleteAtIndex = function(index) {
     if (index < 0 || index >= this.size || this.head === null) return;
     if (index === 0) {
@@ -63,4 +58,27 @@ MyLinkedList.prototype.deleteAtIndex = function(index) {
         }
     }
     this.size--;
+};
+
+//middle of linked list 
+function MiddleNode(head) {
+    let slow = head;
+    let fast = head;
+    while(fast && fast.next) {
+        slow = slow.next;
+        fast = fast.next.next;
+    }
+    return slow;
+}
+
+var reverseList = function(head) {
+    let prev = null;
+    let curr = head;
+    while(curr) {
+        let temp = curr.next;
+        curr.next = prev;
+        prev= curr;
+        curr = temp;
+    }
+    return prev;
 };
