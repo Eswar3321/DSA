284b8eaf4df3698176e0bbdc1ed467235d9e2dca
Eswar3321 <eswararao8786@gmail.com>
2025-07-04 21:51:32 +0530

    rotate list

--- linkedlist.js
+++ linkedlist.js
@@ -276,3 +276,27 @@ var mergeTwoLists = function(list1, list2) {
     }
     return resHead.next;
 };
+
+var rotateRight = function(head, k) {
+    if (!head || !head.next || k === 0) return head;
+    let length = 1;
+    let tail = head;
+    while (tail.next) {
+        tail = tail.next;
+        length++;
+    }
+
+    k = k % length;
+    if (k === 0) return head;
+
+    let stepsToNewHead = length - k;
+    let prev = null;
+    let curr = head;
+    while (stepsToNewHead--) {
+        prev = curr;
+        curr = curr.next;
+    }
+    prev.next = null;
+    tail.next = head;
+    return curr;
+};
