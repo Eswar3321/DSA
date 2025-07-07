b7717b52e10f7c69c812fb6a4799d1db5d8fbb4a
Eswar3321 <eswararao8786@gmail.com>
2025-07-07 21:40:16 +0530

    jewels and stones

--- strings.js
+++ strings.js
@@ -47,3 +47,15 @@ var findWordsContaining = function(words, x) {
     }
     return res;
 };
+
+var numJewelsInStones = function(jewels, stones) {
+    const set = new Set();
+    let count = 0;
+    for(let i=0; i<jewels.length; i++) {
+      set.add(jewels[i]);
+    }
+    for(let i=0; i<=stones.length; i++) {
+    if(set.has(stones[i])) count++;
+    }
+    return count;
+};
