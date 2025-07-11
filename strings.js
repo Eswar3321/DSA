cbd1e2251f75bab647295e4a76c4242077a8692e
Eswar3321 <eswararao8786@gmail.com>
2025-07-11 23:21:28 +0530

    is Palindrome #125

--- strings.js
+++ strings.js
@@ -107,3 +107,41 @@ var reverseStr = function (s, k) {
       }
       return s.join("")
   };  
+
+
+// isPalindrome approach-1
+var isPalindrome = function(s) {
+  s = s.toLowerCase();
+  let filterString = "";
+  let rev = "";
+  for(let i = 0; i < s.length; i++) {
+    if(s[i].match(/[a-z0-9]/i)) {
+      filterString = filterString + s[i];
+      rev = s[i] + rev;
+    }
+  }
+  return filterString === rev;
+}
+
+// isPalindrome Approach-2
+var isPalindrome = function(s) {
+    s = s.toLowerCase();
+    let i = 0;
+    let j = s.length - 1;
+    while (i < j) {
+        if (!s[i].match(/[a-z0-9]/i)) {
+            ++i;
+        }
+        else if (!s[j].match(/[a-z0-9]/i)) {
+            --j;
+        }
+        else if (s[i] === s[j]) {
+            ++i;
+            --j;
+        }
+        else {
+            return false;
+        }
+    }
+    return true;
+  };
