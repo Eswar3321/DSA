aa3338aa1b06e107343d26f2482a56e80196b13d
Eswar3321 <eswararao8786@gmail.com>
2025-07-18 20:41:02 +0530

    group anagrams

--- strings.js
+++ strings.js
@@ -223,23 +223,20 @@ var longestCommonPrefix = function(strs) {
       return Object.values(map);
   };
 
-  // groupAnagrams Approach-2
+// groupAnagrams Approach-2
 var groupAnagrams = function(strs) {
       let map = {};
       for (let i = 0; i < strs.length; i++) {
           let freqArr = Array(26).fill(0);
           let s = strs[i];
-  
           for (let j = 0; j < s.length; j++) {
               let index = s[j].charCodeAt(0) - 'a'.charCodeAt(0);
               freqArr[index]++;
           }
-  
           let key = "";
           for (let k = 0; k < 26; k++) {
               key += "#" + freqArr[k];
           }
-  
           if (!map[key]) {
               map[key] = [s];
           } else {
@@ -248,4 +245,3 @@ var groupAnagrams = function(strs) {
       }
       return Object.values(map);
   };
-  
