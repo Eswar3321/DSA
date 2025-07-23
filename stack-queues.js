d41e3e5eae9cbc0890e6debbbcafa91c36c227ab
Eswar3321 <eswararao8786@gmail.com>
2025-07-23 21:26:06 +0530

    Implement Queue using stacks

--- stack-queues.js
+++ stack-queues.js
@@ -68,3 +68,32 @@ MyStack.prototype.top = function() {
 MyStack.prototype.empty = function() {
     return this.q.length === 0;
 };
+
+// Implement Queue using stacks
+var MyQueue = function() {
+    this.s1 = [];
+    this.s2 = [];
+};
+MyQueue.prototype.push = function(x) {
+    this.s1.push(x);
+};
+MyQueue.prototype.pop = function() {
+    if(this.s2.length === 0) {
+        while(this.s1.length) {
+            this.s2.push(this.s1.pop());
+        }
+    }
+    return this.s2.pop();
+};
+MyQueue.prototype.peek = function() {
+        if(this.s2.length === 0) {
+        while(this.s1.length) {
+            this.s2.push(this.s1.pop());
+        }
+    }
+    return this.s2[this.s2.length-1];
+    
+};
+MyQueue.prototype.empty = function() {
+    return this.s1.length === 0 && this.s2.length === 0;
+};
