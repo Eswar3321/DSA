2ff8fc401b13b46dd642c79266bf2213d0d27725
Eswar3321 <eswararao8786@gmail.com>
2025-08-28 21:40:23 +0530

    day56 find peak and find min

--- binary-search.js
+++ binary-search.js
@@ -78,3 +78,40 @@ var solution = function(isBadVersion) {
     };
 };
 
+// #162 find peak element
+var findPeakElement = function(nums) {
+    let l = 0;
+    let r = nums.length-1;
+    while(l < r) {
+        let m = l + Math.floor((r-l)/2);
+        if(nums[m] < nums[m+1]) {
+            l = m+1;
+        } else {
+            r = m;
+        }
+    }
+    return l;
+};
+
+// #153 find minimum in rotated sorted array
+var findMin = function(a) {
+    let l = 0;
+    let r = a.length - 1;
+
+    while (l <= r) {
+        if (a[l] <= a[r]) {
+            return a[l];
+        }
+
+        let m = l + Math.floor((r - l) / 2);
+        if (a[m] < a[m - 1]) {
+            return a[m];
+        }
+
+        if (a[l] > a[m]) {
+            r = m - 1;
+        } else {
+            l = m + 1;
+        }
+    }
+};
