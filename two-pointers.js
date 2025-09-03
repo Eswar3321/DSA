f5ed7e14919b7e5d4fc5806223d7a3d7476dc3ae
Eswar3321 <eswararao8786@gmail.com>
2025-09-03 22:52:33 +0530

    Find the Index of the First Occurrence in a String

--- two-pointers.js
+++ two-pointers.js
@@ -36,3 +36,60 @@ var isSubsequence = function(s, t) {
     }
     return i === s.length;
 };
+
+// 28. Find the Index of the First Occurrence in a String
+// Input: haystack = "sadbutsad", needle = "sad"
+// Output: 0
+// Explanation: "sad" occurs at index 0 and 6.
+// Time complexity O(n*m)
+var strStr = function(haystack, needle) {
+    for(let i=0; i<haystack.length; i++) {
+        if(needle[0] === haystack[i]) {
+            for(let j=0; j<needle.length; j++) {
+                if(haystack[j+i] !== needle[j]) break;
+                if(j === needle.length-1) return i;
+            }
+        }
+    }
+    return -1;
+};
+
+var strStr = function(haystack, needle) {
+    let n = haystack.length;
+    let m = needle.length;
+    let lps = [0];
+    let i = 0;
+    let j = 1;
+    while(j < m){
+        if(needle[i] === needle[j]) {
+            lps[j] = i+1;
+            ++i;
+            ++j;
+        } else {
+            if(i == 0) {
+                lps[j] = 0;
+                ++j;
+            } else{
+                i = lps[i-1];
+            }
+        }
+    }
+    i = j = 0;
+    while(i < n) {
+        if(haystack[i] === needle[j]) {
+            ++i;
+            ++j;
+        } else{
+            if(j==0) {
+                ++i;
+            } else {
+                j = lps[j-1];
+            }
+        }
+        if(j === m){
+            return i - m;
+        }
+    }
+    return -1;
+};
+     
