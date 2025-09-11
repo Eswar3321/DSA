ccd0cb3b7ae21b0fd8d6654bd15d94eeab2f7d45
Eswar3321 <eswararao8786@gmail.com>
2025-09-11 20:19:22 +0530

    Binary Tree Level Order Traversal

--- trees.js
+++ trees.js
@@ -1,10 +1,10 @@
 // 144. Binary Tree Preorder Traversal-Rucursive Approach
 // Input: root = [1,2,3,4,5,null,8,null,null,6,7,9]
 // Output: [1,2,4,5,6,7,3,8,9]
-var preorderTraversal = function(root) {
+var preorderTraversal = function (root) {
     let res = [];
     function traversal(curr) {
-        if(!curr) return
+        if (!curr) return
         res.push(curr.val);
         traversal(curr.left);
         traversal(curr.right);
@@ -16,10 +16,10 @@ var preorderTraversal = function(root) {
 // 94. Binary Tree Inorder Traversal-Rucursive Approach
 // Input: root = [1,2,3,4,5,null,8,null,null,6,7,9]
 // Output: [4,2,6,5,7,1,3,9,8]
-var inorderTraversal = function(root) {
+var inorderTraversal = function (root) {
     let res = [];
     function traversal(curr) {
-        if(!curr) return;
+        if (!curr) return;
         traversal(curr.left);
         res.push(curr.val);
         traversal(curr.right);
@@ -31,10 +31,10 @@ var inorderTraversal = function(root) {
 // 145. Binary Tree Postorder Traversal-Rucursive Approach
 // Input: root = [1,2,3,4,5,null,8,null,null,6,7,9]
 // Output: [4,6,7,5,2,9,8,3,1]
-var postorderTraversal = function(root) {
+var postorderTraversal = function (root) {
     let res = [];
     function traversal(curr) {
-        if(!curr) return;
+        if (!curr) return;
         traversal(curr.left);
         traversal(curr.right);
         res.push(curr.val);
@@ -46,11 +46,11 @@ var postorderTraversal = function(root) {
 // 144. Binary Tree Preorder Traversal-Interactive Approach
 // Input: root = [1,2,3,4,5,null,8,null,null,6,7,9]
 // Output: [1,2,4,5,6,7,3,8,9]
-var preorderTraversal = function(root) {
-    if(!root) return [];
+var preorderTraversal = function (root) {
+    if (!root) return [];
     let res = [];
     let stack = [root];
-    while(stack.length) {
+    while (stack.length) {
         let curr = stack.pop();
         res.push(curr.val);
         curr.right && stack.push(curr.right);
@@ -62,13 +62,13 @@ var preorderTraversal = function(root) {
 // 94. Binary Tree Inorder Traversal-Interactive Approach
 // Input: root = [1,2,3,4,5,null,8,null,null,6,7,9]
 // Output: [4,2,6,5,7,1,3,9,8]
-var inorderTraversal = function(root) {
+var inorderTraversal = function (root) {
     let ans = [];
     let stack = [];
     let curr = root;
 
-    while(curr || stack.length) {
-        while(curr) {
+    while (curr || stack.length) {
+        while (curr) {
             stack.push(curr);
             curr = curr.left;
         }
@@ -82,18 +82,18 @@ var inorderTraversal = function(root) {
 // 145. Binary Tree Postorder Traversal-Interactive Two stack
 // Input: root = [1,2,3,4,5,null,8,null,null,6,7,9]
 // Output: [4,6,7,5,2,9,8,3,1]
-var postorderTraversal = function(root) {
-    if(!root) return [];
+var postorderTraversal = function (root) {
+    if (!root) return [];
     let s1 = [root];
     let s2 = [];
-    while(s1.length) {
+    while (s1.length) {
         let curr = s1.pop();
         s2.push(curr);
         curr?.left && s1.push(curr.left);
         curr?.right && s1.push(curr.right);
     }
     let res = [];
-    while(s2.length) {
+    while (s2.length) {
         res.push(s2.pop().val);
     }
     return res;
@@ -102,23 +102,62 @@ var postorderTraversal = function(root) {
 // 145. Binary Tree Postorder Traversal-Interactive One stack
 // Input: root = [1,2,3,4,5,null,8,null,null,6,7,9]
 // Output: [4,6,7,5,2,9,8,3,1]
-var postorderTraversal = function(root) {
+var postorderTraversal = function (root) {
     let stack = [];
     let curr = root;
     let ans = [];
     let lastVisisted;
-    while(curr || stack.length) {
-        while(curr) {
+    while (curr || stack.length) {
+        while (curr) {
             stack.push(curr);
             curr = curr.left;
         }
-        let peekNode = stack[stack.length-1];
-        if(peekNode.right && peekNode.right !== lastVisisted) {
+        let peekNode = stack[stack.length - 1];
+        if (peekNode.right && peekNode.right !== lastVisisted) {
             curr = peekNode.right;
         } else {
             ans.push(peekNode.val);
             lastVisisted = stack.pop();
         }
     }
+    return ans;
+};
+
+
+// 102. Binary Tree Level Order Traversal--Iterative Approach
+// Input: root = [3,9,20,null,null,15,7]
+// Output: [[3],[9,20],[15,7]]
+var levelOrder = function(root) {
+    if (!root) return [];
+    let ans = [];
+    let q = [root]; 
+    while (q.length) {
+        let size = q.length;
+        let level = [];
+        for (let i = 0; i < size; i++) {
+            let node = q.shift();
+            level.push(node.val);
+
+            if (node.left) q.push(node.left);
+            if (node.right) q.push(node.right);
+        }
+        ans.push(level);
+    }
+    return ans;
+};
+
+// 102. Binary Tree Level Order Traversal--Recursive Approach
+// Input: root = [3,9,20,null,null,15,7]
+// Output: [[3],[9,20],[15,7]]
+var levelOrder = function(root) {
+    if(!root) return [];
+    let ans = [];
+    let traversal = (curr, level) => {
+        if(!ans[level]) ans[level] = [];
+        ans[level].push(curr.val);
+        curr.left && traversal(curr.left, level + 1);
+        curr.right && traversal(curr.right, level + 1);
+    }
+    traversal(root, 0);
     return ans;
 };
