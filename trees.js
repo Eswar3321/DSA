4a2aca65f5c190784712ceecb5244abec536c998
Eswar3321 <eswararao8786@gmail.com>
2025-09-12 23:21:33 +0530

    path sum

--- trees.js
+++ trees.js
@@ -43,7 +43,7 @@ var postorderTraversal = function (root) {
     return res;
 };
 
-// 144. Binary Tree Preorder Traversal-Interactive Approach
+// 144. Binary Tree Preorder Traversal-Iterative Approach
 // Input: root = [1,2,3,4,5,null,8,null,null,6,7,9]
 // Output: [1,2,4,5,6,7,3,8,9]
 var preorderTraversal = function (root) {
@@ -59,7 +59,7 @@ var preorderTraversal = function (root) {
     return res;
 };
 
-// 94. Binary Tree Inorder Traversal-Interactive Approach
+// 94. Binary Tree Inorder Traversal-Iterative Approach
 // Input: root = [1,2,3,4,5,null,8,null,null,6,7,9]
 // Output: [4,2,6,5,7,1,3,9,8]
 var inorderTraversal = function (root) {
@@ -79,7 +79,7 @@ var inorderTraversal = function (root) {
     return ans;
 };
 
-// 145. Binary Tree Postorder Traversal-Interactive Two stack
+// 145. Binary Tree Postorder Traversal-Iterative Two stack
 // Input: root = [1,2,3,4,5,null,8,null,null,6,7,9]
 // Output: [4,6,7,5,2,9,8,3,1]
 var postorderTraversal = function (root) {
@@ -99,7 +99,7 @@ var postorderTraversal = function (root) {
     return res;
 };
 
-// 145. Binary Tree Postorder Traversal-Interactive One stack
+// 145. Binary Tree Postorder Traversal-Iterative One stack
 // Input: root = [1,2,3,4,5,null,8,null,null,6,7,9]
 // Output: [4,6,7,5,2,9,8,3,1]
 var postorderTraversal = function (root) {
@@ -160,4 +160,68 @@ var levelOrder = function(root) {
     }
     traversal(root, 0);
     return ans;
+};
+
+// 104. Maximum Depth of Binary Tree
+// Input: root = [3,9,20,null,null,15,7]
+// Output: 3
+var maxDepth = function(root) {
+    if(!root) return 0;
+    let maxLevel = 0;
+    let traversal = (curr, level) => {
+        maxLevel = Math.max(level, maxLevel);
+        curr.left && traversal(curr.left, level+1);
+        curr.right && traversal(curr.right, level+1);
+    }
+    traversal(root, 1);
+    return maxLevel;
+};
+// 112. Path Sum
+// Input: root = [5,4,8,11,null,13,4,7,2,null,null,null,1], targetSum = 22
+// Output: true
+// Explanation: The root-to-leaf path with the target sum is shown.
+var hasPathSum = function(root, targetSum) {
+    if(!root) return 0;
+    let traversal = (curr, currSum) => {
+        let newSum = currSum + curr.val;
+        if(!curr.left && !curr.right) {
+            if(newSum === targetSum) {
+                return true;
+            }
+        }
+        return curr.left && traversal(curr.left, newSum) || 
+        curr.right && traversal(curr.right, newSum) || false;
+    }
+    return traversal(root, 0);
+};
+
+// 101. Symmetric Tree--Recursive Approach
+// Input: root = [1,2,2,3,4,4,3]
+// Output: true
+var isSymmetric = function(root) {
+    let isMirror = (left, right) => {
+        if(!left && !right) return true;
+        if(!left || !right) return false;
+        return left.val === right.val && 
+        isMirror(left.left, right.right) && 
+        isMirror(left.right, right.left); 
+    }
+    return isMirror(root.left, root.right);
+};
+
+// 101. Symmetric Tree--Iterative Approach
+// Input: root = [1,2,2,3,4,4,3]
+// Output: true
+var isSymmetric = function(root) {
+    let q = [root.left, root.right];
+    while(q.length) {
+        let p1 = q.shift();
+        let p2 = q.shift();
+        if(!p1 && !p2) continue;
+        if(!p1 || !p2) return false;
+        if(p1.val !== p2.val) return false;
+        q.push(p1.left, p2.right);
+        q.push(p1.right, p2.left);
+    }
+    return true;
 };
