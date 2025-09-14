d0dc9caf13556375af2d368d6d01b6f5d46165f1
Eswar3321 <eswararao8786@gmail.com>
2025-09-14 16:47:32 +0530

    Binary Tree Right Side View

--- trees.js
+++ trees.js
@@ -127,10 +127,10 @@ var postorderTraversal = function (root) {
 // 102. Binary Tree Level Order Traversal--Iterative Approach
 // Input: root = [3,9,20,null,null,15,7]
 // Output: [[3],[9,20],[15,7]]
-var levelOrder = function(root) {
+var levelOrder = function (root) {
     if (!root) return [];
     let ans = [];
-    let q = [root]; 
+    let q = [root];
     while (q.length) {
         let size = q.length;
         let level = [];
@@ -149,11 +149,11 @@ var levelOrder = function(root) {
 // 102. Binary Tree Level Order Traversal--Recursive Approach
 // Input: root = [3,9,20,null,null,15,7]
 // Output: [[3],[9,20],[15,7]]
-var levelOrder = function(root) {
-    if(!root) return [];
+var levelOrder = function (root) {
+    if (!root) return [];
     let ans = [];
     let traversal = (curr, level) => {
-        if(!ans[level]) ans[level] = [];
+        if (!ans[level]) ans[level] = [];
         ans[level].push(curr.val);
         curr.left && traversal(curr.left, level + 1);
         curr.right && traversal(curr.right, level + 1);
@@ -165,13 +165,13 @@ var levelOrder = function(root) {
 // 104. Maximum Depth of Binary Tree
 // Input: root = [3,9,20,null,null,15,7]
 // Output: 3
-var maxDepth = function(root) {
-    if(!root) return 0;
+var maxDepth = function (root) {
+    if (!root) return 0;
     let maxLevel = 0;
     let traversal = (curr, level) => {
         maxLevel = Math.max(level, maxLevel);
-        curr.left && traversal(curr.left, level+1);
-        curr.right && traversal(curr.right, level+1);
+        curr.left && traversal(curr.left, level + 1);
+        curr.right && traversal(curr.right, level + 1);
     }
     traversal(root, 1);
     return maxLevel;
@@ -180,17 +180,17 @@ var maxDepth = function(root) {
 // Input: root = [5,4,8,11,null,13,4,7,2,null,null,null,1], targetSum = 22
 // Output: true
 // Explanation: The root-to-leaf path with the target sum is shown.
-var hasPathSum = function(root, targetSum) {
-    if(!root) return 0;
+var hasPathSum = function (root, targetSum) {
+    if (!root) return 0;
     let traversal = (curr, currSum) => {
         let newSum = currSum + curr.val;
-        if(!curr.left && !curr.right) {
-            if(newSum === targetSum) {
+        if (!curr.left && !curr.right) {
+            if (newSum === targetSum) {
                 return true;
             }
         }
-        return curr.left && traversal(curr.left, newSum) || 
-        curr.right && traversal(curr.right, newSum) || false;
+        return curr.left && traversal(curr.left, newSum) ||
+            curr.right && traversal(curr.right, newSum) || false;
     }
     return traversal(root, 0);
 };
@@ -198,13 +198,13 @@ var hasPathSum = function(root, targetSum) {
 // 101. Symmetric Tree--Recursive Approach
 // Input: root = [1,2,2,3,4,4,3]
 // Output: true
-var isSymmetric = function(root) {
+var isSymmetric = function (root) {
     let isMirror = (left, right) => {
-        if(!left && !right) return true;
-        if(!left || !right) return false;
-        return left.val === right.val && 
-        isMirror(left.left, right.right) && 
-        isMirror(left.right, right.left); 
+        if (!left && !right) return true;
+        if (!left || !right) return false;
+        return left.val === right.val &&
+            isMirror(left.left, right.right) &&
+            isMirror(left.right, right.left);
     }
     return isMirror(root.left, root.right);
 };
@@ -212,14 +212,14 @@ var isSymmetric = function(root) {
 // 101. Symmetric Tree--Iterative Approach
 // Input: root = [1,2,2,3,4,4,3]
 // Output: true
-var isSymmetric = function(root) {
+var isSymmetric = function (root) {
     let q = [root.left, root.right];
-    while(q.length) {
+    while (q.length) {
         let p1 = q.shift();
         let p2 = q.shift();
-        if(!p1 && !p2) continue;
-        if(!p1 || !p2) return false;
-        if(p1.val !== p2.val) return false;
+        if (!p1 && !p2) continue;
+        if (!p1 || !p2) return false;
+        if (p1.val !== p2.val) return false;
         q.push(p1.left, p2.right);
         q.push(p1.right, p2.left);
     }
@@ -229,37 +229,37 @@ var isSymmetric = function(root) {
 // 226. Invert Binary Tree
 // Input: root = [4,2,7,1,3,6,9]
 // Output: [4,7,2,9,6,3,1]
-var invertTree = function(root) {
-   if(!root) return root;
-   let temp = root.left;
-   root.left = root.right;
-   root.right = temp;
-   invertTree(root.left);
-   invertTree(root.right);
-   return root;
+var invertTree = function (root) {
+    if (!root) return root;
+    let temp = root.left;
+    root.left = root.right;
+    root.right = temp;
+    invertTree(root.left);
+    invertTree(root.right);
+    return root;
 };
 
 // 100. Same Tree
 // Input: p = [1,2,3], q = [1,2,3]
 // Output: true
-var isSameTree = function(p, q) {
-    if(!p && !q) return true;
-    if(!p || !q) return false;
-    return p.val === q.val && 
-    isSameTree(p.left, q.left) && 
-    isSameTree(p.right, q.right);
+var isSameTree = function (p, q) {
+    if (!p && !q) return true;
+    if (!p || !q) return false;
+    return p.val === q.val &&
+        isSameTree(p.left, q.left) &&
+        isSameTree(p.right, q.right);
 };
 
 // 110. Balanced Binary Tree
 // Input: root = [3,9,20,null,null,15,7]
 // Output: true
-var isBalanced = function(root) {
+var isBalanced = function (root) {
     let ans = true;
     let calculateHeight = (curr) => {
-        if(!curr) return 0;
+        if (!curr) return 0;
         let leftHeight = calculateHeight(curr.left);
         let rightHeight = calculateHeight(curr.right);
-        if(Math.abs(leftHeight - rightHeight) > 1) {
+        if (Math.abs(leftHeight - rightHeight) > 1) {
             ans = false;
         }
         return 1 + Math.max(leftHeight, rightHeight);
@@ -272,10 +272,10 @@ var isBalanced = function(root) {
 // Input: root = [1,2,3,4,5]
 // Output: 3
 // Explanation: 3 is the length of the path [4,2,1,3] or [5,2,1,3]
-var diameterOfBinaryTree = function(root) {
+var diameterOfBinaryTree = function (root) {
     let maxDiameter = 0;
     let findDepth = (curr) => {
-        if(!curr) return 0;
+        if (!curr) return 0;
         let leftDepth = findDepth(curr.left);
         let rightDepth = findDepth(curr.right);
         let currDepth = leftDepth + rightDepth;
@@ -284,4 +284,112 @@ var diameterOfBinaryTree = function(root) {
     }
     findDepth(root);
     return maxDiameter;
+};
+
+// 103. Binary Tree Zigzag Level Order Traversal--Recursive
+// Input: root = [3,9,20,null,null,15,7]
+// Output: [[3],[20,9],[15,7]]
+var zigzagLevelOrder = function (root) {
+    if (!root) return [];
+    let res = [];
+    let traversal = (curr, level) => {
+        if (!res[level]) res[level] = [];
+        if (level % 2 === 1) res[level].unshift(curr.val);
+        else res[level].push(curr.val);
+        curr.left && traversal(curr.left, level + 1);
+        curr.right && traversal(curr.right, level + 1);
+    };
+    traversal(root, 0);
+    return res;
+};
+// 103. Binary Tree Zigzag Level Order Traversal--Iterative
+var zigzagLevelOrder = function(root) {
+    if(!root) return [];
+    let res = [];
+    let level = 0;
+    let q = [root];
+    while(q.length) {
+        let levelArray = [];
+        let levelSize = q.length;
+        for(let i = 0; i < levelSize; i++) {
+            let curr = q.shift();
+            if(level%2===1) levelArray.unshift(curr.val);
+            else levelArray.push(curr.val);
+            curr.left && q.push(curr.left);
+            curr.right && q.push(curr.right);
+        }
+        res.push(levelArray);
+        level++;       
+    }
+    return res;
+};
+// 572. Subtree of Another Tree
+// Input: root = [3,4,5,1,2], subRoot = [4,1,2]
+// Output: true
+var isSubtree = function(root, subRoot) {
+    let hashRoot = serialize(root);
+    let hashSubRoot = serialize(subRoot);
+    return hashRoot.includes(hashSubRoot);
+};
+let serialize = function(root) {
+  let hash = "";
+  let traversal = (curr) => {
+    if(!curr) {
+        hash += "-#";
+        return;
+    }
+    hash = hash + '-' + curr.val;
+    traversal(curr.left);
+    traversal(curr.right);
+  }
+  traversal(root);
+  return hash;
+}
+// 236. Lowest Common Ancestor of a Binary Tree
+// Input: root = [3,9,20,null,null,15,7]
+// Output: [[3],[9,20],[15,7]]
+var lowestCommonAncestor = function(root, p, q) {
+    let lca = null;
+    let traversal = (curr) => {
+        let count = 0;
+        if(!curr) return 0;
+        let ansOnLeft = traversal(curr.left);
+        let ansOnRight = traversal(curr.right);
+        if(p.val === curr.val || q.val === curr.val) ++count;
+        count = count + ansOnLeft + ansOnRight;
+        if(count === 2 && !lca) lca = curr;
+        return count;
+    }
+    traversal(root);
+    return lca;
+};
+// 199. Binary Tree Right Side View-Iterative
+// Input: root = [1,2,3,null,5,null,4]
+// Output: [1,3,4]
+var rightSideView = function(root) {
+    if(!root) return [];
+    let q = [root];
+    let res = [];
+    while(q.length) {
+        let levelSize = q.length;
+        for(let i=0; i<levelSize; i++) {
+            let curr = q.shift();
+            i == 0 && res.push(curr.val);
+            curr.right && q.push(curr.right);
+            curr.left && q.push(curr.left);
+        }
+    }
+    return res;
+};
+// 199. Binary Tree Right Side View-Recursive
+var rightSideView = function(root) {
+    let res = [];
+    let traversal = (curr, level) => {
+        if(!curr) return;
+        if(res.length === level) res.push(curr.val);
+        traversal(curr.right, level+1);
+        traversal(curr.left, level+1);
+    }
+    traversal(root, 0);
+    return res;
 };
